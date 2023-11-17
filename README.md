f58436666bc1dd249f47d01a5485d93b174ea7b4
ikarabulut <i.karabulut93@gmail.com>
2023-11-16 22:40:14 -0600

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Machine Learning on Graphs for NLP
+
+## Subprojects
+
+- `/ncbi-scraper`: web scraper for[National Center for Biotechnology Information : Safety and Health at Work journal](https://www.ncbi.nlm.nih.gov/pmc/journals/1859/) downloading all publications pdf's
+
+  **PDF's are located in `/pmc`**
---
+++ ncbi-scraper/main.py
@@ -0,0 +1,42 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+I = 0
+ROOT_URL = "https://www.ncbi.nlm.nih.gov"
+URL = "https://www.ncbi.nlm.nih.gov/pmc/journals/1859/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+job_elements = soup.find_all("a", class_="arc-issue", href=True)
+
+publication_urls = []
+for job_element in job_elements:
+    publication_urls.append(ROOT_URL + job_element["href"])
+
+for publication_url in publication_urls:
+    publications_page = requests.get(publication_url)
+    p_soup = BeautifulSoup(publications_page.content, "html.parser")
+    rpt_divs = p_soup.find_all(
+        "div", class_="rprt")
+    for link_div in rpt_divs:
+        links_div = link_div.find_all(
+            "div", class_="links")
+        for link in links_div:
+            links = link.find_all(
+                "a", class_="view", href=True, string=lambda text: "pdf" in text.lower())
+            for link in links:
+                href = link["href"]
+                user_agent = "scrapping_script/1.0"
+                headers = {'User-Agent': user_agent}
+                pdf = response = requests.get(
+                    ROOT_URL + href, headers=headers, stream=True)
+                current_directory = os.getcwd()
+                final_directory = os.path.join(
+                    current_directory, f'{href.lstrip("/").replace("/main.pdf", "")}')
+                os.makedirs(final_directory)
+                with open(final_directory + "/main.pdf", 'wb') as f:
+                    f.write(response.content)
+                    print(f"Files downloaded: {I}")
+                    I += 1
