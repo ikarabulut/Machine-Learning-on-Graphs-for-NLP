9fc66dbdd520def46d3b8359c7692977e0b93aba
ikarabulut <i.karabulut93@gmail.com>
2023-11-29 16:30:17 -0600

    wip

---
+++ vector-etl/main.py
@@ -0,0 +1,38 @@
+import boto3
+from llama_index import download_loader
+import nbib
+from llama_index.vector_stores import PineconeVectorStore
+import pinecone
+import os
+
+
+client = boto3.client("s3")
+S3Reader = download_loader("S3Reader")
+
+
+bucket = "ncbi-safetyandhealth-nbib"
+key = "PMC10024226.nbib"
+
+# paginator = client.get_paginator('list_objects')
+# page_iterator = paginator.paginate(Bucket=bucket)
+# for page in page_iterator:
+#     for obj in page['Contents']:
+#         loader = S3Reader(bucket=bucket, key=f"{obj['Key']}")
+#         print(f"Loading {obj['Key']}")
+#         documents = loader.load_data()
+
+loader = S3Reader(bucket=bucket, key=key)
+documents = loader.load_data()
+refs = nbib.read(documents[0].text)
+ref_abstract = refs[0]["abstract"]
+
+
+pinecone.init(api_key=os.environ['PINECONE_API_KEY'],
+              environment="gcp-starter")
+
+try:
+    pinecone.create_index("ncib-safetyandhealth-abstracts", dimension=1536,
+                          metric="cosine", pod_type="Starter")
+except Exception:
+    pass
+pinecone_index = pinecone.Index("ncib-safetyandhealth-abstracts")
