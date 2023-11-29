7bc04a2174bb5f93accdd99b5623d7b932cf4f09
ikarabulut <i.karabulut93@gmail.com>
2023-11-29 00:16:34 -0600

    add workflow

--- ncbi-scraper/main.py
+++ ncbi-scraper/main.py
@@ -1,96 +1,74 @@
 import requests
 import os
-import sys
 import json
 from typing import List
 from bs4 import BeautifulSoup
 
 from models.article import Article, ArticleList
 
-mode = sys.argv[1]
 I = 0
 ROOT_URL = "https://www.ncbi.nlm.nih.gov"
 URL = "https://www.ncbi.nlm.nih.gov/pmc/journals/1859/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-user_agent = "scrapping_script/1.0"
+user_agent = "scrapping_script/1.5"
 headers = {'User-Agent': user_agent}
 
-if mode == "abstract":
-    article_models = ArticleList
-    article_models.articles = []
+article_models = ArticleList
+article_models.articles = []
 
-    job_elements = soup.find_all("a", class_="arc-issue", href=True)
-    publication_urls = []
-    # loop through each publication
-    for job_element in job_elements:
-        publication_urls.append(ROOT_URL + job_element["href"])
+job_elements = soup.find_all("a", class_="arc-issue", href=True)
+publication_urls = []
+# loop through each publication
+for job_element in job_elements:
+    publication_urls.append(ROOT_URL + job_element["href"])
     # loop through each publication article
-    for publication_url in publication_urls:
-        article_model = Article
-        publications_page = requests.get(publication_url)
-        p_soup = BeautifulSoup(publications_page.content, "html.parser")
-        publicationlink_divs = p_soup.find_all(
-            "div", class_="title")
-        for link in publicationlink_divs:
-            publication_link = link.find("a", class_="view",  href=True)
-            href = publication_link["href"]
-            print(ROOT_URL + href)
-            article = requests.get(ROOT_URL + href, headers=headers)
-            article_soup = BeautifulSoup(
-                article.content, "html.parser")
+for publication_url in publication_urls:
+    publications_page = requests.get(publication_url)
+    p_soup = BeautifulSoup(publications_page.content, "html.parser")
+    publicationlink_divs = p_soup.find_all(
+        "div", class_="title")
+    for link in publicationlink_divs:
+        publication_link = link.find("a", class_="view",  href=True)
+        href = publication_link["href"]
+        print(ROOT_URL + href)
+        article = requests.get(ROOT_URL + href, headers=headers)
+        article_soup = BeautifulSoup(
+            article.content, "html.parser")
 
-            try:
-                abstract = article_soup.find("div", id="abs0010")
-                abstract_text = abstract.find_all("p", class_="p p-first-last")
-            except AttributeError:
-                abstract = article_soup.find("div", id="body-a.g")
-                abstract_text = abstract.find_all(
-                    "div", {"id": lambda L: L and L.startswith('P0')})
-            abstract_text_sb = ""
-            for text in abstract_text:
-                abstract_text_sb += text.text
-            article_model.abstract = abstract_text_sb
+        try:
+            # abstract = article_soup.find("div", id="abs0010")
+            abstract = article_soup.find("div",
+                                         {"id": lambda L: L and L.startswith('abs')})
+            abstract_text = abstract.find_all("p", class_="p p-first-last")
+        except AttributeError:
+            print(f"No Abstract for article {ROOT_URL + href}")
+            continue
+        abstract_text_sb = ""
+        for text in abstract_text:
+            abstract_text_sb += text.text
+        cite_button = article_soup.find("button", class_="citation-button")
+        citation_response = requests.get(
+            ROOT_URL + cite_button["data-all-citations-url"], headers=headers)
+        cite_json = json.loads(citation_response.content)
+        print(cite_json["id"])
 
-            cite_button = article_soup.find("button", class_="citation-button")
-            citation_response = requests.get(
-                ROOT_URL + cite_button["data-all-citations-url"], headers=headers)
-            cite_json = json.loads(citation_response.content)
-            article_model.pmid = cite_json["id"]
-            article_model.cite = cite_json["nlm"]["format"]
+        pdf = requests.get(
+            ROOT_URL + href + "pdf/main.pdf", headers=headers, stream=True)
+        current_directory = os.getcwd()
+        final_directory = os.path.join(
+            current_directory, f'{href.lstrip("/").replace("/main.pdf", "")}')
+        os.makedirs(final_directory)
+        with open(final_directory + "/main.pdf", 'wb') as f:
+            f.write(pdf.content)
+            print(f"Files downloaded: {I}")
+            I += 1
+        location = href + "pdf/main.pdf"
+        article_model = Article(
+            location="s3://ncbi-safetyandhealth-pdfs" + location, pmid=cite_json["id"], cite=cite_json["nlm"]["format"], abstract=abstract_text_sb)
+        print(f"ID:: {article_model.pmid} created")
+        article_models.articles.append(article_model)
 
-            print(f"ID:: {article_model.pmid} created")
-            article_models.articles.append(article_model)
-    print(json.dumps(article_models, indent=2))
-else:
-    job_elements = soup.find_all("a", class_="arc-issue", href=True)
-    publication_urls = []
-    for job_element in job_elements:
-        publication_urls.append(ROOT_URL + job_element["href"])
-
-    for publication_url in publication_urls:
-        publications_page = requests.get(publication_url)
-        p_soup = BeautifulSoup(publications_page.content, "html.parser")
-        rpt_divs = p_soup.find_all(
-            "div", class_="rprt")
-        for link_div in rpt_divs:
-            links_div = link_div.find_all(
-                "div", class_="links")
-            for link in links_div:
-                links = link.find_all(
-                    "a", class_="view", href=True, string=lambda text: "pdf" in text.lower())
-                for link in links:
-                    href = link["href"]
-                    user_agent = "scrapping_script/1.0"
-                    headers = {'User-Agent': user_agent}
-                    pdf = response = requests.get(
-                        ROOT_URL + href, headers=headers, stream=True)
-                    current_directory = os.getcwd()
-                    final_directory = os.path.join(
-                        current_directory, f'{href.lstrip("/").replace("/main.pdf", "")}')
-                    os.makedirs(final_directory)
-                    with open(final_directory + "/main.pdf", 'wb') as f:
-                        f.write(response.content)
-                        print(f"Files downloaded: {I}")
-                        I += 1
+with open(os.getcwd() + "/publications_dump.json", "w") as f:
+    f.write(article_models.model_dump_json())
--- ncbi-scraper/models/article.py
+++ ncbi-scraper/models/article.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 
 class Article(BaseModel):
-    pmid: str
-    location: str
-    cite: str
-    abstract: str
+    pmid: Optional[str]
+    location: Optional[str]
+    cite: Optional[str]
+    abstract: Optional[str]
 
 
 class ArticleList(BaseModel):
